15fc6be00ef67983424038a0f4c3e73d00ba5c8e
Joâo victor <jv2644099@gmail.com>
2023-11-13 17:31:03 -0300

    feitas

---
+++ decisao3.py
@@ -0,0 +1,10 @@
+# questão 1
+
+a = int(input("Insira o primeiro valor inteiro: "))
+b = int(input("Insira o segundo valor inteiro: "))
+if a>b:
+    print(f'{a} é maior que {b}')
+if b>a:
+    print(f'{b} é maior que {a}')
+if a==b:
+    print(f'{a} é igual à {b}')
